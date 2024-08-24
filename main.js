dfc2a218dcd9ee4d3688eef8d4f921aaaa76955e
yuki5718 <hayate8575@gmail.com>
2024-08-24 21:59:57 +0800

    新增頁面-關於我們

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+// import Swiper from 'swiper';            // import Swiper JS
+// import 'swiper/css';                    // import Swiper styles
 import './public/swiper.js'
 
 console.log("Hello world!");
